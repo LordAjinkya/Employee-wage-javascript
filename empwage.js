775e1556201d7d1b16a5bfffa8053aaed6640fb6
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-06-07 10:54:13 +0530

    Added:code to check attendance

--- empwage.js
+++ empwage.js
@@ -1,1 +1,8 @@
 console.log('Welcome to employee wage program');
+
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if  (empCheck == IS_ABSENT)
+    console.log("Employee is ABSENT");
+else
+    console.log("Employee Is PRESENT");

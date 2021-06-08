8470e2582ee9ce0db862e6c32a8e4690c346f4ee
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-06-08 11:14:54 +0530

    Added:code to use arrow function to use filter,reduce methods

--- empwage.js
+++ empwage.js
@@ -14,6 +14,7 @@ let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 let empDailyWageArr = new Array();
 let empDailyWageMap = new Map();
+let empDailyHrsMap = new Map();
 
 
 function calcDailyWage(empHrs){
@@ -28,7 +29,7 @@ while ( totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS
     totalEmpHrs += empHrs;
     empDailyWageArr.push(calcDailyWage(empHrs));
     empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
-
+    empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
 }   
 //Function For Getting Working Hours
 function getWorkingHours(empCheck){
@@ -48,5 +49,28 @@ console.log(empDailyWageMap);
 function totalWages(totalWages, dailyWage){
     return totalWages + dailyWage;
 }
+//UC-8
 console.log("UC8- Emp Wage Map totalHrs: "+
                 Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+ 
+//UC-9
+const findTotal = (totalVal, dailyVal) => {
+    return totalVal + dailyVal;
+
+}
+let count = 0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal, 0);
+let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(findTotal, 0);
+console.log("UC9A - Emp Wage with Arrow.:" + " Total Hours: " + totalHours + " Total Wages : " + totalSalary);
+
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach( (values , key ,map) => {
+    if( values == 8 ) fullWorkingDays.push(key);
+    else if (values == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+console.log("Full Working Days: " +fullWorkingDays);
+console.log("Part Working Days: " +partWorkingDays);
+console.log("Non Working Days: " +nonWorkingDays);                
